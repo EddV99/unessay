192ad5fe2828faf75161406c9e5fd3724e6bf454
Eduardo Valdivia <eddval0808@gmail.com>
2024-04-06 14:14:59 -0600

    Initial commit, site structure done

---
+++ website/main.js
@@ -0,0 +1,60 @@
+import './style.css'
+
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
+const FOV = 65;
+const NEAR = 0.1;
+const FAR = 100;
+
+const canvas = document.getElementById('game-canvas');
+
+//const WIDTH = canvas.clientWidth * window.devicePixelRatio;
+//const HEIGHT = canvas.clientHeight * window.devicePixelRatio;
+const WIDTH = window.innerWidth;
+//const HEIGHT = window.innerHeight;
+const HEIGHT = 1000;
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(FOV, WIDTH / HEIGHT, NEAR, FAR);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(WIDTH, HEIGHT);
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({color: 0XC28300});
+const torus = new THREE.Mesh(geometry, material);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0,0,20);
+
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+//const controls = new OrbitControls(camera, renderer.domElement);
+
+//scene.add(lightHelper);
+//scene.add(gridHelper);
+scene.add(pointLight);
+scene.add(ambientLight);
+scene.add(torus);
+
+
+function animate(){
+    requestAnimationFrame(animate); 
+    torus.rotation.x += 0.01;
+    torus.rotation.z += 0.01;
+    torus.rotation.y += 0.001;
+
+//     controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
